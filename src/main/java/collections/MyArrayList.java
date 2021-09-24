223013e502d9b58b6cb6d1a1dd0600625a63b742
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-09-24 13:41:39 +0300

    refactoring after comments

--- src/main/java/collections/MyArrayList.java
+++ src/main/java/collections/MyArrayList.java
@@ -3,8 +3,6 @@ package collections;
 public class MyArrayList<E> {
     private static final int DEFAULT_CAPACITY = 10;
     private E[] elementData;
-    // TODO: переменные size и index взаимозаменяемы, от одной из них можно избавиться
-    private int size;
     private int index;
 
     public MyArrayList() {
@@ -16,30 +14,25 @@ public class MyArrayList<E> {
     }
 
     public void add(E value) {
-        // TODO: здесь лучше поставить index >= elementData.length, понятно, что вряд ли в данной реализации индекс окажется больше размера массива, но на всякий случай все обычно так делают
-        if (index == elementData.length) {
+        if (index >= elementData.length) {
             growCapacity();
         }
         elementData[index] = value;
         index++;
-        size++;
     }
 
     public int indexOf(E value) {
         int result = -1;
         for (int i = 0; i < index; i++) {
             if (elementData[i].equals(value)) {
-                // TODO: здесь можно сразу написать return i;
-                result = i;
-                break;
+                return i;
             }
         }
         return result;
     }
 
-    // TODO: лучше назвать метод getSize()
-    public int size() {
-        return size;
+    public int getSize() {
+        return index;
     }
 
     public E get(int index) {
@@ -51,7 +44,6 @@ public class MyArrayList<E> {
         checkIndex(index);
         System.arraycopy(elementData, index + 1,
                 elementData, index, this.index - index);
-        size--;
         this.index--;
 
         return true;
@@ -66,13 +58,12 @@ public class MyArrayList<E> {
                 elementData, index + 1, this.index - index);
         elementData[index] = value;
         this.index++;
-        size++;
 
         return true;
     }
 
     public void print() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < index; i++) {
             System.out.println(elementData[i]);
         }
     }
